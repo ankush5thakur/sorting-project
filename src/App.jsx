6204f541ba609ae0597095272c2a4fdc3bec5a01
ANKUSH THAKUR <ankukush05@gmail.com>
2024-07-15 01:00:49 +0530

    bubble sort done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import { Component } from "react";
 import "./App.css";
+// algo...
+import BubbleSort from "./algorithms/BSort.jsx";
+import Bar from "./components/Bar.jsx";
+import Play from "@mui/icons-material/PlayCircleOutlineRounded";
+import Forward from "@mui/icons-material/SkipNextRounded";
+import Backward from "@mui/icons-material/SkipPreviousRounded";
+import RotateLeft from "@mui/icons-material/RotateLeft";
 
 class App extends Component {
   state = {
@@ -8,40 +15,173 @@ class App extends Component {
     colorKey: [],
     colorSteps: [],
     currentStep: 0,
-    count: 10,
+    count: 20,
     delay: 100,
-    algorithm: "",
+    algorithm: "Bubble Sort",
     timeouts: [],
+  };
+
+  ALGORITHMS = {
+    "Bubble Sort": BubbleSort,
   };
 
   componentDidMount() {
     this.generateRandomArray();
   }
 
+  generateSteps = () => {
+    let array = this.state.array.slice();
+    let steps = this.state.arraySteps.slice();
+    let colorSteps = this.state.colorSteps.slice();
+    this.ALGORITHMS[this.state.algorithm](array, 0, steps, colorSteps);
+
+    this.setState({
+      arraySteps: steps,
+      colorSteps: colorSteps,
+    });
+  };
+
+  clearTimeouts = () => {
+    this.state.timeouts.forEach((timeout) => clearTimeout(timeout));
+    this.setState({
+      timeouts: [],
+    });
+  };
+
+  clearColorKey = () => {
+    let blankKey = new Array(this.state.count).fill(0);
+    this.setState({
+      colorKey: blankKey,
+      colorSteps: [blankKey],
+    });
+  };
+
   generateRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
   };
 
-  generateRandomArray() {
-    const { count } = this.state; // Destructure count from state
+  generateRandomArray = () => {
+    this.clearTimeouts();
+    this.clearColorKey();
+    const { count } = this.state;
     const temp = [];
 
     for (let i = 0; i < count; i++) {
       temp.push(this.generateRandomNumber(50, 200));
     }
 
-    // Update the state with the generated random array
-    this.setState({ array: temp }, () => {
-      console.log(this.state.array); // Log the updated array
+    this.setState({ array: temp, arraySteps: [temp], currentStep: 0 }, () => {
+      console.log(this.state.array);
+      this.generateSteps();
     });
-  }
+  };
+
+  changeArray = (index, value) => {
+    let arr = this.state.array.slice();
+    arr[index] = value;
+    this.setState(
+      {
+        array: arr,
+        arraySteps: [arr],
+        currentStep: 0,
+      },
+      () => {
+        this.generateSteps();
+      }
+    );
+  };
+
+  previousStep = () => {
+    let currentStep = this.state.currentStep;
+    if (currentStep === 0) return;
+    currentStep -= 1;
+    this.setState({
+      currentStep: currentStep,
+      array: this.state.arraySteps[currentStep],
+      colorKey: this.state.colorSteps[currentStep],
+    });
+  };
+
+  nextStep = () => {
+    let currentStep = this.state.currentStep;
+    if (currentStep >= this.state.arraySteps.length - 1) return;
+    currentStep += 1;
+    this.setState({
+      currentStep: currentStep,
+      array: this.state.arraySteps[currentStep],
+      colorKey: this.state.colorSteps[currentStep],
+    });
+  };
+
+  start = () => {
+    let steps = this.state.arraySteps;
+    let colorSteps = this.state.colorSteps;
+
+    this.clearTimeouts();
+
+    let timeouts = [];
+    let i = this.state.currentStep;
+
+    while (i < steps.length) {
+      let timeout = setTimeout(() => {
+        this.setState((prevState) => ({
+          array: steps[prevState.currentStep],
+          colorKey: colorSteps[prevState.currentStep],
+          currentStep: prevState.currentStep + 1,
+        }));
+      }, this.state.delay * (i - this.state.currentStep));
+      timeouts.push(timeout);
+      i++;
+    }
+    this.setState({
+      timeouts: timeouts,
+    });
+  };
 
   render() {
+    let bars = this.state.array.map((value, index) => (
+      <Bar
+        key={index}
+        index={index}
+        length={value}
+        color={this.state.colorKey[index]}
+        changeArray={this.changeArray}
+      />
+    ));
+
+    let playButton;
+
+    if (this.state.arraySteps.length === this.state.currentStep) {
+      playButton = (
+        <button className="controller" onClick={this.generateRandomArray}>
+          <RotateLeft />
+        </button>
+      );
+    } else {
+      playButton = (
+        <button className="controller" onClick={this.start}>
+          <Play />
+        </button>
+      );
+    }
+
     return (
       <div className="app">
-        <h1>har har mahadev</h1>
-        <p>Generated Array: {JSON.stringify(this.state.array)}</p>{" "}
-        {/* Display the array */}
+        <div className="frame">
+          <div className="barsDiv container card">{bars}</div>
+        </div>
+        <div className="control-panel">
+          <div className="control-buttons">
+            <button className="controller" onClick={this.previousStep}>
+              <Backward />
+            </button>
+            {playButton}
+            <button className="controller" onClick={this.nextStep}>
+              <Forward />
+            </button>
+          </div>
+        </div>
+        <div className="panel"></div>
       </div>
     );
   }
---
+++ src/components/Bar.jsx
@@ -0,0 +1,114 @@
+import "./Bar.css";
+import { useEffect, useState } from "react";
+
+function Bar({ index, length, color, changeArray }) {
+  const [len, setLen] = useState(length);
+  useEffect(() => {
+    setLen(length);
+  }, [length]);
+  const colors = [
+    ["rgba(61,90,241,0.5)", "rgba(61,90,241,0.2)"],
+    ["rgba(255,48,79,1)", "rgba(255,48,79,0.5)"],
+    ["rgba(131,232,90,0.5)", "rgba(131,232,90,0.2)"],
+  ];
+
+  const inputStyle = {
+    position: "absolute",
+    top: "50%", // Center vertically
+    left: "50%", // Center horizontally
+    transform: "translate(-50%, -50%) rotate(-90deg)", // Centering with rotation
+    border: "none",
+    width: "50px",
+    background: "none",
+  };
+
+  const Bottom = {
+    transform: `translateY(${200 - len}px) rotateX(-90deg)`,
+    backgroundColor: colors[color][0],
+    boxShadow: `5px 5px 50px 5px ${colors[color][1]}`,
+    transition: "0.3s",
+  };
+
+  const frontbackrightLeft = {
+    height: `${len}px`,
+    transform: `translateY(${200 - len}px)`,
+    backgroundColor: colors[color][0],
+    boxShadow: `5px 5px 50px 5px ${colors[color][1]}`,
+    transition: "0.3s",
+  };
+
+  const handleChange = (e) => {
+    let val = e.target.value;
+    if (val === "") {
+      setLen(0);
+      changeArray(index, 0);
+    } else {
+      val = parseInt(val);
+      if (val > 200) {
+        setLen(200);
+        changeArray(index, 200);
+      } else {
+        setLen(val);
+        changeArray(index, val);
+      }
+    }
+  };
+  const handleIncrement = () => {
+    setLen(len + 1);
+    changeArray(index, len);
+  };
+
+  const handleDecrement = () => {
+    setLen(len - 1);
+    changeArray(index, len);
+  };
+
+  return (
+    <div className="bar">
+      <div className="side top"></div>
+      <div className="side bottom" style={Bottom}></div>
+      <div className="side right">
+        <div
+          className="color-bar right-color-bar"
+          style={frontbackrightLeft}
+        ></div>
+      </div>
+      <div className="side left">
+        <div
+          className="color-bar left-color-bar"
+          style={frontbackrightLeft}
+        ></div>
+      </div>
+      <div className="side front">
+        <div className="color-bar front-color-bar" style={frontbackrightLeft}>
+          <input
+            type="number"
+            value={len}
+            className="input"
+            style={inputStyle}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+      <div className="side back">
+        <div
+          className="color-bar back-color-bar"
+          style={frontbackrightLeft}
+        ></div>
+      </div>
+      <div className="quantity-nav">
+        <div className="quantity-button quantity-up" onClick={handleIncrement}>
+          +
+        </div>
+        <div
+          className="quantity-button quantity-down"
+          onClick={handleDecrement}
+        >
+          -
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Bar;
